bb37cbc3271136235a10dbf7559109aff215ea75
neo-level <frederik@neo-level.com>
2022-12-04 18:05:23 +0100

    replaced duplicate out direct

--- mk-outs.sh
+++ mk-outs.sh
@@ -6,5 +6,5 @@ OUTDIR="tests/expected"
 echo "Hello there" > $OUTDIR/hello1.txt
 echo "Hello"   "there" > $OUTDIR/hello2.txt
 echo -n "Hello   there" > $OUTDIR/hello1.n.txt
-echo -n "Hello" "there" > $OUTDIR/hello1.n.txt
+echo -n "Hello" "there" > $OUTDIR/hello2.n.txt
 
