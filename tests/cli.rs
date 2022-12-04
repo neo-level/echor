eb65386264ce5474c3e0e92a261c7fcaa303c153
neo-level <frederik@neo-level.com>
2022-12-04 17:14:45 +0100

    test added to run the program with no arguments and assert if it fails -> prints usage to STDERR

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,2 +1,11 @@
 use assert_cmd::Command;
 use predicates::prelude::*;
+
+#[test]
+fn dies_no_args() {
+    Command::cargo_bin("echor")
+        .unwrap()
+        .assert()
+        .failure()
+        .stderr(predicate::str::contains("USAGE"));
+}
