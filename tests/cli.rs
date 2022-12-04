e1aa6274810ba9c299fd0e131748fb4afb78a94c
neo-level <frederik@neo-level.com>
2022-12-04 17:33:56 +0100

    created object on heap to store the result of the test, success -> unit type, fail std::error. implemented a helper function to prevent code duplication.

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,5 +1,17 @@
 use assert_cmd::Command;
 use predicates::prelude::*;
+use std::fs;
+
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+
+fn run(args: &[&str], file: &str) -> TestResult {
+    let expected = fs::read_to_string(file)?;
+    Command::cargo_bin("echor")?
+        .assert()
+        .failure()
+        .stderr(predicate::str::contains("USAGE"));
+    Ok(())
+}
 
 #[test]
 fn dies_no_args() {
